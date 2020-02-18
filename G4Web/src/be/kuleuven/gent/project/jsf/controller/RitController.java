fc0bcc788f22b9ee1c9dcda71cecb4ab7fdab0e5
dairo <dairo@10.108.16.178>
2020-02-18 13:13:39 +0100

    Begin RitController en UserController

---
+++ G4Web/src/be/kuleuven/gent/project/jsf/controller/RitController.java
@@ -0,0 +1,27 @@
+package be.kuleuven.gent.project.jsf.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import be.kuleuven.gent.project.data.Rit;
+import be.kuleuven.gent.project.ejb.TaskManagementEJBLocal;
+import be.kuleuven.gent.project.ejb.UserManagementEJBLocal;
+
+@Named
+@ViewScoped
+public class RitController {
+	private static final long serialVersionUID = -3737221385235612830L;
+	@EJB
+	private RitManagementEJBLocal taskEJB;
+	
+	@EJB
+	private UserManagementEJBLocal userEJB;
+	
+	private Rit rit = new Rit();
+	
+	
+}
--- G4Web/src/be/kuleuven/gent/project/jsf/controller/UserController.java
+++ G4Web/src/be/kuleuven/gent/project/jsf/controller/UserController.java
@@ -1,5 +1,23 @@
 package be.kuleuven.gent.project.jsf.controller;
 
-public class UserController {
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+
+@Named
+@
+public class UserController implements Serializable{
+	private static final long serialVersionUID = 6737147724536164355L;
 	//test 4
+	
+	//activate de roles
+	//logout
+	
+	
 }
