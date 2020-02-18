b9f65d4aa2d772616bba2e7accdef32f3e8930b8
Sam <Sam@10.108.17.37>
2020-02-18 14:14:23 +0100

    Route db-classe toegevoegt

--- G4JPA/src/be/kuleuven/gent/project/data/Profiel.java
+++ G4JPA/src/be/kuleuven/gent/project/data/Profiel.java
@@ -8,6 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SecondaryTable;
 import javax.persistence.Table;
+
+import com.sun.jmx.snmp.Timestamp;
+
 import javax.persistence.PrimaryKeyJoinColumn;
 
 
@@ -21,4 +24,25 @@ public class Profiel implements Serializable {
 	
 	@Column(name="name", nullable=false)
 	private String name;
+
+	@Column(name="prijs", nullable=false)
+	private int prijs;
+	
+	@Column(name="betaald", nullable=false)
+	private boolean betaald;
+	
+	@Column(name="aantal_personen", nullable=false)
+	private int aantalPersonen;
+	
+	@Column(name="vertrektijd", nullable=false)
+	private Timestamp vertrektijd;
+	
+	@Column(name="eindtijd", nullable=false)
+	private Timestamp eindtijd;
+	
+	@Column(name="passagier", nullable=false)
+	private Profiel passagier;
+	
+	@Column(name="route", nullable=false)
+	private Route route;
 }
---
+++ G4JPA/src/be/kuleuven/gent/project/data/Route.java
@@ -0,0 +1,48 @@
+package be.kuleuven.gent.project.data;
+import java.io.Serializable;
+import java.text.DecimalFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SecondaryTable;
+import javax.persistence.Table;
+
+import com.sun.jmx.snmp.Timestamp;
+
+@Entity
+@Table(name="Route")
+public class Route {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="idRoute", nullable=false,length=16)
+	private int id;
+	
+	@Column(name="beginpunt_lat", nullable=false)
+	private DecimalFormat beginpuntLat;
+	
+	@Column(name="beginpunt_long", nullable=false)
+	private DecimalFormat beginpuntLong;
+	
+	@Column(name="eindpunt_lat", nullable=false)
+	private DecimalFormat eindpuntLat;
+	
+	@Column(name="eindpunt_long", nullable=false)
+	private DecimalFormat eindpuntLong;
+	
+	@Column(name="eindtijd", nullable=false)
+	private Timestamp eindtijd;
+	
+	@Column(name="vertrektijd", nullable=false)
+	private Timestamp vertrektijd;
+	
+	@Column(name="bestuurder", nullable=false)
+	private Profiel bestuurder;
+
+	
+	@Column(name="route", nullable=false)
+	private Route route;
+
+}
