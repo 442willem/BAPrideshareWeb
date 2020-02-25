8c4dbafb553b834c80a524b0b9479a6c0c55c535
Sam <Sam@DESKTOP-4FRQ768>
2020-02-25 09:13:01 +0100

    Profiel getters/setters toegevoegd

--- G4JPA/src/be/kuleuven/gent/project/data/Profiel.java
+++ G4JPA/src/be/kuleuven/gent/project/data/Profiel.java
@@ -49,6 +49,86 @@ public class Profiel implements Serializable {
 	@Column(name="password", nullable=false)
 	private String password;
 	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getDriverscore() {
+		return driverscore;
+	}
+
+	public void setDriverscore(int driverscore) {
+		this.driverscore = driverscore;
+	}
+
+	public int getPassagierscore() {
+		return passagierscore;
+	}
+
+	public void setPassagierscore(int passagierscore) {
+		this.passagierscore = passagierscore;
+	}
+
+	public String getVoornaam() {
+		return voornaam;
+	}
+
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
+
+	public String getAchternaam() {
+		return achternaam;
+	}
+
+	public void setAchternaam(String achternaam) {
+		this.achternaam = achternaam;
+	}
+
+	public String getStad() {
+		return stad;
+	}
+
+	public void setStad(String stad) {
+		this.stad = stad;
+	}
+
+	public String getStraat() {
+		return straat;
+	}
+
+	public void setStraat(String straat) {
+		this.straat = straat;
+	}
+
+	public int getHuisnummer() {
+		return huisnummer;
+	}
+
+	public void setHuisnummer(int huisnummer) {
+		this.huisnummer = huisnummer;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public Profiel() {}
 	
 	public Profiel(String stad,String straat,int huisnummer,String login, String password,String achternaam, String voornaam) {
