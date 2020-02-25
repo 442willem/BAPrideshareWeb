b0717fdcf0ec0e098ddbc4944e496c5723b2e38a
wille <willem.juwet@gmail.com>
2020-02-25 09:13:30 +0100

    tussenstopmanagement

---
+++ G4EJB/ejbModule/be/kuleuven/gent/project/ejb/TussenstopManagementEJB.java
@@ -0,0 +1,20 @@
+package be.kuleuven.gent.project.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class TussenstopManagementEJB
+ */
+@Stateless
+@LocalBean
+public class TussenstopManagementEJB implements TussenstopManagementEJBLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public TussenstopManagementEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
