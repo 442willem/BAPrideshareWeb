05dbe1db005767e43d90b8460b216b8e2b25f73d
Sam <Sam@DESKTOP-4FRQ768>
2020-02-25 11:08:05 +0100

    profiel random commentaar bijgezet

--- G4JPA/src/be/kuleuven/gent/project/data/Profiel.java
+++ G4JPA/src/be/kuleuven/gent/project/data/Profiel.java
@@ -15,6 +15,7 @@ import javax.persistence.PrimaryKeyJoinColumn;
 @Table(name="profiel")
 @SecondaryTable(name = "profiel_Group", pkJoinColumns=@PrimaryKeyJoinColumn(name="login", referencedColumnName="login"))
 public class Profiel implements Serializable {
+	//alle parameters die in de DB staan
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="idProfiel", nullable=false,length=16)
