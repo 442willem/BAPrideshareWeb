3e747820b22aa8a5a0d14e92492a58f159bcd42c
wille <willem.juwet@gmail.com>
2020-02-25 09:34:15 +0100

    RouteManagementEJB

--- G4EJB/ejbModule/be/kuleuven/gent/project/ejb/RouteManagementEJB.java
+++ G4EJB/ejbModule/be/kuleuven/gent/project/ejb/RouteManagementEJB.java
@@ -2,6 +2,10 @@ package be.kuleuven.gent.project.ejb;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import be.kuleuven.gent.project.data.Route;
 
 /**
  * Session Bean implementation class RouteManagementEJB
@@ -9,12 +13,16 @@ import javax.ejb.Stateless;
 @Stateless
 @LocalBean
 public class RouteManagementEJB implements RouteManagementEJBLocal {
-
+	@PersistenceContext(unitName="db")
+	private EntityManager em;
     /**
      * Default constructor. 
      */
     public RouteManagementEJB() {
         // TODO Auto-generated constructor stub
+    }
+    public void createRoute(Route r) {
+    	em.persist(r);
     }
 
 }
--- G4EJBClient/ejbModule/be/kuleuven/gent/project/ejb/RouteManagementEJBLocal.java
+++ G4EJBClient/ejbModule/be/kuleuven/gent/project/ejb/RouteManagementEJBLocal.java
@@ -2,7 +2,9 @@ package be.kuleuven.gent.project.ejb;
 
 import javax.ejb.Local;
 
+import be.kuleuven.gent.project.data.Route;
+
 @Local
 public interface RouteManagementEJBLocal {
-
+	public void createRoute(Route r);
 }
