cb478551b93f58bef8c6ed51f08917f4439ddfdd
dairo <dairo@MSI>
2020-03-03 12:03:24 +0100

    Create route

--- G4Web/src/be/kuleuven/gent/project/jsf/controller/UserController.java
+++ G4Web/src/be/kuleuven/gent/project/jsf/controller/UserController.java
@@ -39,6 +39,9 @@ public class UserController implements Serializable{
 	public String goToTest() {
 		return "test.faces?faces-redirect=true";
 	}
+	public String goToCreateRoute() {
+		return "driver/createRoute.faces?faces-redirect=true";
+	}
 	
 	
 	public void logout() {
