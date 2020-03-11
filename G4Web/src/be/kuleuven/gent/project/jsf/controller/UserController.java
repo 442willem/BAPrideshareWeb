51d29f8ae295e473a2d1454efd3ae98174606484
dairo <dairo@MSI>
2020-03-11 09:58:45 +0100

    kleine aanpassingen+ een home knop

--- G4Web/src/be/kuleuven/gent/project/jsf/controller/UserController.java
+++ G4Web/src/be/kuleuven/gent/project/jsf/controller/UserController.java
@@ -48,7 +48,9 @@ public class UserController implements Serializable{
 	public String goToMyRoutes() {
 		return "myRoutes.faces?faces-redirect=true";
 	}
-	
+	public String goToIndexD() {
+		return "indexD.faces?faces-redirect=true&login=1";
+	}
 	
 
 }
